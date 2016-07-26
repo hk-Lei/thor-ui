92d3cfa91a358196e3428fd8b6d4c9afe93e8372
crazyjingling <mh39092207@gmail.com>
2016-07-27 01:26:34 +0800

    add send email with shell

--- util/sendEmail.js
+++ util/sendEmail.js
@@ -1,9 +1,9 @@
 'use strict';
 const nodemailer = require("nodemailer");
-
+const child_process = require('child_process');
 const transporter = nodemailer.createTransport('smtps://1321812120%40qq.com:Node.js1413@smtp.qq.com');
 
-exports.sendEmail = function (errNodes) {
+const sendEmail = function (errNodes) {
 	errNodes = errNodes || [];
 	errNodes = errNodes.map((item)=>{
 		return 	`<li>${item}</li>`;
@@ -17,11 +17,28 @@ exports.sendEmail = function (errNodes) {
 			html : `<div><p>这是 thor-ui 发出的通知,您有服务器出现故障,请及时查看修复!</p></div><ul>${errNodes}</ul>`
 		}, function(error, response){
 			if(error){
-				console.log('send email fail');
+				console.log('send email failed');
 			}else{
 				console.log("Message sent: " + response.message);
 			}
 			transporter.close();
 		});
 	}
-}
+};
+const sendEmailWithShell = function (errNodes) {
+	errNodes = errNodes || [];
+	if(errNodes.length){
+		errNodes = errNodes.joim('\n    ');
+
+		child_process.exec(`echo -e "这是 thor-ui 发出的通知,您有服务器出现故障,请及时查看修复！\n    ${errNodes}" | mail -s "thor error" leihuaike@emar.com `, (err, result)=> {
+			if(error){
+				console.log('send email failed');
+			}else{
+				console.log("Message sent: " + response.message);
+			}
+			process.disconnect();
+		});
+	}
+};
+exports.sendEmail = sendEmail;
+exports.sendEmailWithShell = sendEmailWithShell;
